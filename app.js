e6a13c76348c5d4b9693b2172f8f72a04037b4f8
Roman Biaskrouny <Nike72by@gmail.com>
2024-05-19 20:27:22 +0300

    New commit

--- app.js
+++ app.js
@@ -1,38 +1,85 @@
 const inputText = document.getElementById('input-text')
 const btnAddNote = document.getElementById('btn-add-note')
-const btnSaveNote = document.getElementById('btn-save-note')
+const btnCheckNote = document.getElementById('btn-check-note')
 const btnDeleteNote = document.getElementById('btn-delete-note')
 const spanText = document.getElementById('span-text')
 const ulElement = document.getElementById('ul-note')
 
-const notes = []
+const notes = [{
+    title: "Hello",
+    completed: true
+},
+{
+    title: "Noob",
+    completed: false
+}]
 
-function addNote(title) {
-    if (title.length === 0) {
-        return
+function render() {
+    ulElement.innerHTML=''
+    for (let [index, element] of notes.entries()) {
+        ulElement.insertAdjacentHTML('beforeend', addNoteTemplete(index, element))
     }
-    ulElement.insertAdjacentHTML(
-        'beforeend',
+    if (notes.length === 0) {
+        ulElement.insertAdjacentHTML('beforeend',         
         `
-            <li>
-                <div class="note-text">
-                    <span id="span-text">${title}</span>
-                    <div class="btn-notes">
-                        <button id="btn-save-note">
-                            <img src="images/checkbox.png" alt="remove" />
-                        </button>
-                        <button id="btn-delete-note">
-                            <img src="images/icon-delete.png" alt="check" />
-                        </button>
-                    </div>
-                </div>
-            </li>
-        `  
+            <p>No notes</p>
+        `
     )
-    notes.push(title)
+    }
+}
+render()
+
+function addNoteTemplete(index, note) {
+    return  `
+                <li>
+                    <div class="note-text">
+                        <span id="${note.completed ? 'span-text-crossed-out' : 'span-text'}">${note.title}</span>
+                        <div class="btn-notes">
+                            <button id="${note.completed ? 'btn-check-note-true' : 'btn-check-note-false'}"
+                            data-type="check"
+                            data-index="${index}">
+                                <img src="images/icon-checkbox.png" alt="check"
+                                data-type="check"
+                                data-index="${index}"/>
+                            </button>
+                            <button id="btn-delete-note"
+                            data-type="delete"
+                            data-index="${index}">
+                                <img src="images/icon-delete.png" alt="delete" 
+                                data-type="delete"
+                                data-index="${index}"/>
+                            </button>
+                        </div>
+                    </div>
+                </li>
+            `  
 }
 
-btnAddNote.onclick = function () {
-    addNote(inputText.value)
+btnAddNote.onclick = function addNote() {
+    if (inputText.value.length === 0) {
+        return
+    }
+    const newNote = {
+        title: inputText.value,
+        completed: false
+    }
+    notes.push(newNote)
+    render()
     inputText.value = ''
+}
+
+ulElement.onclick = function (event) {
+    console.log(event.target.dataset.index)
+    if (event.target.dataset.index) {
+        const index = event.target.dataset.index
+        const type = event.target.dataset.type
+
+        if (type === "check") {
+            notes[index].completed = !notes[index].completed
+        }
+        else if (type === "delete") {
+            notes.splice(index,1)
+        }
+        render()
+    }
 }
